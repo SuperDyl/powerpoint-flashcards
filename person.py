2caaf6fb1ba66fef9c1bf020a05a7bbd48b1aa7e
Dylan <SuperDyl19@gmail.com>
2022-03-17 14:23:36 -0600

    Projects now split

--- person.py
+++ person.py
@@ -1,7 +1,7 @@
 """
 Stores data for a person
 """
-
+from room import Room
 
 class Person:
     def __init__(self, name: str, room: str, picture: str = "TODO"):  #TODO: add picture support
@@ -14,3 +14,11 @@ class Person:
 
     def __repr__(self):
         return str(self)
+
+    @classmethod  # TODO:Remove from Person class
+    def from_string(cls, string):
+        *name, room = string.split(', ')
+        prof = Person(', '.join(name), Room.from_string(room))
+        cls.name = prof.name
+        cls.room = prof.room
+        return cls
