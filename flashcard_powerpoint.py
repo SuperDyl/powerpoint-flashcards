3e57a9c67b3788ddd11b5462ee9e7f5d8c01dea5
Dylan <SuperDyl19@gmail.com>
2022-03-18 13:59:40 -0600

    Started down the original path of using methods for creating ecah slide

--- flashcard_powerpoint.py
+++ flashcard_powerpoint.py
@@ -12,43 +12,44 @@ BLANK_LAYOUT = 6
 # FONT_COLOR_MODE = {"RGB": "rgb"}
 
 
-class SlideTemplate:
-    def __init__(self, **kwargs):
-        pass
-
-
 class FlashcardPowerPoint:
-    def __init__(self):
-        self.people = list()
+    def __init__(self, people: list = None):
+        self.people = list(people) if people is not None else list()
         self.name_bottom_padding = Inches(1.58)
         self.font_size = Pt(88)
         self.font_color = RGBColor(255, 0, 0)
         self.font_color_mode = "rgb"  # FONT_COLOR_MODE["RGB"]
 
-    def add_person(self, person: Person):
-        self.people.append(person)
-
-    def _add_room_slide(self, presentation: Presentation, person: Person):
-        pass
-
-    def _add_picture_slide(self, presentation: Presentation, person: Person):
+    def _add_default_slide(self, presentation: Presentation, person_name:string):
         slide = presentation.slides.add_slide(BLANK_LAYOUT)
         textbox_height = self.name_bottom_padding
         name_textbox = slide.shapes.add_textbox(left=0, top=presentation.slide_height - textbox_height,
                                                 width=presentation.slide_width, height=textbox_height)
         frame = name_textbox.text_frame
-        frame.text = person.name
+        frame.text = person_name
         frame.auto_size = MSO_AUTO_SIZE.NONE
         frame.paragraphs[0].alignment = PP_PARAGRAPH_ALIGNMENT.CENTER
         font = frame.paragraphs[0].font
         font.size = self.font_size
         setattr(font.color, self.font_color_mode, self.font_color)
+        return slide
+
+    def _add_room_slide(self, presentation: Presentation, person: Person):
+        slide = self._add_default_slide(presentation, person.name)
+        return slide
+
+    def _add_picture_slide(self, presentation: Presentation, person: Person):
+        slide = self._add_default_slide(presentation, person.name)
+        return slide
 
     def _add_slides(self, presentation: Presentation, person: Person):
         if person.name and person.picture:
             self._add_picture_slide(presentation, person)
         if person.name and person.room:
             self._add_room_slide(presentation, person)
+
+    def add_person(self, person: Person):
+        self.people.append(person)
 
     def export_powerpoint(self, file_name):
         presentation = Presentation()
