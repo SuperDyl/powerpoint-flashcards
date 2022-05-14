625dde2410e9ad76db8df28504eae6c4c41313a4
Dylan <SuperDyl19@gmail.com>
2022-05-13 19:14:25 -0600

    Cleaned out bugs for Windows scripts

--- FlashcardsPowerpoint-win.bat
+++ FlashcardsPowerpoint-win.bat
@@ -1,6 +1,10 @@
+@echo off
 @echo "This file can also be run using command line arguments. Use -h for help"
-.\venv\Scripts\activate.path
+call .\venv\Scripts\activate.bat
 cd src
 @echo "Running professorFlashcards.py with supplied arguments"
-python -m professorFlashcards "%*"
+python -m professorFlashcards %*
+cd ..
+deactivate
 @echo "Done"
+pause
--- install-windows.bat
+++ install-windows.bat
@@ -1,5 +1,15 @@
+@echo off
+@echo "Before continuing, ensure that you have Python3.6 downloaded from Python.org"
+pause
 @echo "Setting up Python libraries in a virtual environment"
-python3 -m venv .venv
-.\venv\Scripts\activate.path
+python -m venv venv
+call .\venv\Scripts\activate.bat
+
+python -m pip install --upgrade pip
+python -m pip install --upgrade setuptools
+python -m pip install --upgrade wheel
+
 python -m pip install -r src\requirements.txt
+deactivate
 @echo "Done setting up the environment"
+pause
