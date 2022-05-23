f16b3a822442d6e40180ed2b9a92e8cef44d63db
Dylan <SuperDyl19@gmail.com>
2022-05-23 13:17:44 -0600

    Removed quotes from @echo and moved deactivate to the end

--- src/FlashcardsPowerpoint-win.bat
+++ src/FlashcardsPowerpoint-win.bat
@@ -1,11 +1,11 @@
 @echo off
-@echo "This file can also be run using command line arguments. Use -h for help"
+@echo This file can also be run using command line arguments. Use -h for help
 call .\venv\Scripts\activate.bat
 cd src
-@echo "Running employeeFlashcards.py with supplied arguments"
-@echo "It may take a couple minutes if re-downloading photos"
-python -m employeeFlashcards %*
+@echo Running employeeFlashcards.py with supplied arguments
+@echo It may take a couple minutes if re-downloading photos
+python -m employeeFlashcards %1 %*
 cd ..
-deactivate
-@echo "Done"
+@echo Done
 pause
+deactivate
