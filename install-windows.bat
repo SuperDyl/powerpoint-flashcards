be0743f9fd48e3810cfa80def2db16744722e9f6
SuperDyl <SuperDyl19@gmail.com>
2022-09-29 09:58:13 -0600

    fixed install procedure for windows

--- install-windows.bat
+++ install-windows.bat
@@ -5,15 +5,13 @@ pause
 @echo Setting up or re-setting up Python libraries in a virtual environment
 rmdir .\venv /s /q
 python -m venv venv
-call .\venv\Scripts\activate.bat
 
-python -m pip install --upgrade pip
-python -m pip install --upgrade setuptools
-python -m pip install --upgrade wheel
+.\venv\Scripts\python.exe -m pip install --upgrade pip
+.\venv\Scripts\python.exe -m pip install --upgrade setuptools
+.\venv\Scripts\python.exe -m pip install --upgrade wheel
 
-python -m pip install -r src\requirements.txt
+.\venv\Scripts\python.exe -m pip install -r src\requirements.txt
 copy /y .\src\FlashcardsPowerpoint-win.bat .\
 @echo Done setting up the environment
 @echo Use FlashcardsPowerpoint-win.bat to create powerpoints
 pause
-deactivate
